eca4033b1ca3b725aeafde4528312a556d38a658
mahiv87 <maherreraiv1987@gmail.com>
2023-08-28 12:38:10 -0600

    debugged courses click

--- script.ts
+++ script.ts
@@ -7,7 +7,7 @@ const email = process.env.EMAIL_ADDRESS;
 const password = process.env.PASSWORD;
 
 async function main() {
-	let driver: WebDriver = await new Builder().forBrowser('chrome').build();
+	const driver: WebDriver = await new Builder().forBrowser('chrome').build();
 
 	await driver.get(`${url}`);
 
@@ -19,6 +19,30 @@ async function main() {
 
 	let sign_in = await driver.findElement(By.id('loginSubmit'));
 	await sign_in.click();
+
+	driver.manage().setTimeouts({ implicit: 5000 });
+
+	let courses_button = await driver.findElement(
+		By.id('global_nav_courses_link')
+	);
+	await courses_button.click();
+
+	let course = await driver.findElement(
+		By.linkText('Bootcamp: UofU-VIRT-FSF-PT-08-2023-U-LOLC-MWTH(A)')
+	);
+	await course.click();
+
+	let zoom_button = await driver.findElement(
+		By.className('context_external_tool_249')
+	);
+	await zoom_button.click();
+
+	driver.manage().setTimeouts({ implicit: 5000 });
+
+	let cloud_button = await driver.findElement(
+		By.xpath("//div[@class='ant-tabs-tab' and text()='Cloud Recordings']")
+	);
+	await cloud_button.click();
 
 	// let currentUrl: string = await driver.getCurrentUrl();
 	// console.log(`Current url: ${currentUrl}`);
