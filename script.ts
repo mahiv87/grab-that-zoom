515950f75c6a2193c81083885943e374de26a62c
mahiv87 <maherreraiv1987@gmail.com>
2023-08-28 11:06:59 -0600

    login code

--- script.ts
+++ script.ts
@@ -3,15 +3,25 @@ import 'dotenv/config';
 import { Builder, By, Key, WebDriver } from 'selenium-webdriver';
 
 const url = process.env.BCS;
+const email = process.env.EMAIL_ADDRESS;
+const password = process.env.PASSWORD;
 
 async function main() {
 	let driver: WebDriver = await new Builder().forBrowser('chrome').build();
 
 	await driver.get(`${url}`);
 
-	let currentUrl: string = await driver.getCurrentUrl();
+	let email_input = await driver.findElement(By.id('emailAddress'));
+	await email_input.sendKeys(`${email}`);
 
-	console.log(`Current url: ${currentUrl}`);
+	let password_input = await driver.findElement(By.id('password'));
+	await password_input.sendKeys(`${password}`);
+
+	let sign_in = await driver.findElement(By.id('loginSubmit'));
+	await sign_in.click();
+
+	// let currentUrl: string = await driver.getCurrentUrl();
+	// console.log(`Current url: ${currentUrl}`);
 
 	await driver.quit();
 }
